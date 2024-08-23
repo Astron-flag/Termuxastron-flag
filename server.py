e0d2669fc63c4e58d2f10b9bbf0ab9a26b7a1fdb
Astron-flag <ggwrv10@gmail.com>
2024-08-23 21:10:06 +0300

    Update server.py

--- server.py
+++ server.py
@@ -7,9 +7,9 @@ from time import sleep
 # Proprietary and confidential
 # Written by Anasov <me@anasov.ly>, 05, May, 2024.
 
-BASE_URL: str = "https://cpmnuker.anasov.ly/api"
+BASE_URL: str = "https://cpmewan.com/api"
 
-class CPMNuker:
+class CPMEwan:
 
     def __init__(self, access_key) -> None:
         self.auth_token = None
